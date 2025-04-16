965bc2e079912366f0e61be3d7dc5fe2daf5c6fe
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2025-04-16 10:33:16 +0200

    jsonrpsee v0.25

--- substrate/client/rpc-servers/src/middleware/mod.rs
+++ substrate/client/rpc-servers/src/middleware/mod.rs
@@ -21,14 +21,14 @@
 use std::{
 	num::NonZeroU32,
 	time::{Duration, Instant},
+	future::Future,
+	convert::Infallible,
 };
 
-use futures::future::{BoxFuture, FutureExt};
 use governor::{clock::Clock, Jitter};
 use jsonrpsee::{
-	server::middleware::rpc::RpcServiceT,
-	types::{ErrorObject, Id, Request},
-	MethodResponse,
+	server::middleware::rpc::{RpcServiceT, Batch, Notification, Request, MethodResponse},
+	types::{ErrorObject, Id},
 };
 
 mod metrics;
@@ -91,19 +91,21 @@ impl<S> tower::Layer<S> for MiddlewareLayer {
 /// because the metrics needs to know whether
 /// a call was rate-limited or not because
 /// it will impact the roundtrip for a call.
+#[derive(Debug, Clone)]
 pub struct Middleware<S> {
 	service: S,
 	rate_limit: Option<RateLimit>,
 	metrics: Option<Metrics>,
 }
 
-impl<'a, S> RpcServiceT<'a> for Middleware<S>
+impl<S> RpcServiceT for Middleware<S>
 where
-	S: Send + Sync + RpcServiceT<'a> + Clone + 'static,
+	S: RpcServiceT<Response = MethodResponse, Error = Infallible> + Send + Sync + Clone + 'static,
 {
-	type Future = BoxFuture<'a, MethodResponse>;
+	type Response = S::Response;
+	type Error = S::Error;
 
-	fn call(&self, req: Request<'a>) -> Self::Future {
+	fn call<'a>(&self, req: Request<'a>) -> impl Future<Output = Result<Self::Response, Self::Error>> + Send + 'a {
 		let now = Instant::now();
 
 		self.metrics.as_ref().map(|m| m.on_call(&req));
@@ -121,7 +123,7 @@ where
 
 				loop {
 					if attempts >= MAX_RETRIES {
-						return reject_too_many_calls(req.id);
+						return Ok(reject_too_many_calls(req.id));
 					}
 
 					if let Err(rejected) = limit.inner.check() {
@@ -137,11 +139,28 @@ where
 			}
 
 			let rp = service.call(req.clone()).await;
-			metrics.as_ref().map(|m| m.on_response(&req, &rp, is_rate_limited, now));
+
+			match rp {
+				Ok(ref rp) => {
+					metrics.as_ref().map(|m| m.on_response(&req, rp, is_rate_limited, now));
+				}
+				Err(e) => match e {}
+			};
 
 			rp
 		}
-		.boxed()
+	}
+
+	fn batch<'a>(&self, batch: Batch<'a>) -> impl Future<Output = Result<Self::Response, Self::Error>> + Send + 'a {
+		self.service.batch(batch)
+	}
+
+
+	fn notification<'a>(
+		&self,
+		n: Notification<'a>,
+	) -> impl Future<Output = Result<Self::Response, Self::Error>> + Send + 'a {
+		self.service.notification(n)
 	}
 }
 
